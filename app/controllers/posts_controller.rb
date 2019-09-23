ab6add6ce54aaf14e5a3ffaebac3dbe10324939b
jamievullo <jvullo@protonmail.com>
2019-09-23 16:09:14 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+  def create 
+
+  end
+
   def show
   end
 
@@ -8,9 +12,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+    
   end
 
   private
